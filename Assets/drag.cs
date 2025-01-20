8df9bf099ae8285b72e33a4106c1af045636b644
MrMuffin <mrrmuffinn@gmail.com>
2025-01-21 01:46:59 +0530

    initial card slot movement

---
+++ Assets/drag.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics.Tracing;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class drag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    public Transform previousslot;
+    //private Vector3 ParentLocalPosition;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    private void Update()
+    {
+        //ParentLocalPosition = transform.parent.localPosition;
+    }
+    public void OnBeginDrag(PointerEventData eventdata)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+        previousslot = transform.parent;
+        eventdata.pointerDrag.transform.SetParent(canvas.gameObject.transform);
+
+    }
+    public void OnDrag(PointerEventData eventdata)
+    {
+        rectTransform.anchoredPosition += eventdata.delta / canvas.scaleFactor;
+    }
+    public void OnEndDrag(PointerEventData eventdata)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+        // transform.localPosition = ParentLocalPosition;
+        if (eventdata.pointerDrag.transform.parent == canvas.gameObject.transform)
+        {
+            eventdata.pointerDrag.transform.SetParent(previousslot.transform);
+        }
+
+        eventdata.pointerDrag.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+    }
+
+
+}
